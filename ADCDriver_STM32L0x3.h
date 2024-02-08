fd06fefa2d2ebab56e5848fc32d99aa9e509b7f7
Balazs Farkas <94067165+BFarkascode@users.noreply.github.com>
2024-02-08 11:13:31 +0100

    Update ADCDriver_STM32L0x3.h

--- ADCDriver_STM32L0x3.h
+++ ADCDriver_STM32L0x3.h
@@ -1,16 +1,10 @@
 /*
- *
  *  Created on: Oct 27, 2023
- *  Project: STM32_ADc-DACriver
- *  File: ADCDriver_custom.h
  *  Author: BalazsFarkas
+ *  Project: STM32_ADC-DACDriver
  *  Processor: STM32L053R8
- *  Compiler: ARM-GCC (STM32 IDE)
- *  Program version: 1.0
- *  Program description: N/A
- *  Hardware description/pin distribution: N/A
- *  Modified from: N/A
- *  Change history: N/A
+ *  Header version: 1.0
+ *  File: ADCDriver_STM32L0x3.h
  */
 
 #ifndef INC_ADCDRIVER_CUSTOM_H_
